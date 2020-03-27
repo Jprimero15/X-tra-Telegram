88d1c4d5ca6b9a3e3710d847ee2792948d9010bc
Joshua Primero <jprimero155@gmail.com>
2020-03-27 03:27:00 -0700

    Y are u gei

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,4 +1,4 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+"""Check if userbot alive..."""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -6,11 +6,11 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet, check pinned in @XtraTgBot"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`====================\nTelethon Version: 6.6.6 \nPython Version: 3.7.3\n"
-                     "`Selinux Status: Enforcing!\n====================\n\n`"
-                     "`My PRO Owner: @Jprimero15`")
+    await alive.edit("`**PARANOID BOT Info:**\n\nTelethon Version: 6.6.6 \nPython Version: 3.7.3\n`"
+                     "`Selinux Status: Enforcing!\n\n`"
+                     "`My PRO Owner:` @Jprimero15")
